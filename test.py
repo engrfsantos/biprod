be8506755062cb2c244e3b9879f1de09afa754d2
Reginaldo Francisco <eng.rfsantos@gmail.com>
2025-07-01 19:35:47 -0300

    Update test.py

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ import psycopg2
 
 # Configurações de conexão
 un = 'tsiuserdb'
-cs = '10.1.5.6:1521/PRODCTRL'
+cs = '10.0.0.1:1521/PRODCTRL'
 pw = os.environ['PW_ORACLE']
 
 os.environ['PYO_SAMPLES_ORACLE_CLIENT_PATH'] = os.getenv("ORACLE_HOME") 
 
 #with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
-oracle_conn_str = "tsiuserdb@10.1.5.6:1521/PRODCTRL"
-postgres_conn_str = "dbname='producao' user='producao' password='producao' host='172.16.0.40' port='5441'"
+oracle_conn_str = "userdb@10.0.0.1:1521/PRODCTRL"
+postgres_conn_str = "dbname='producao' user='user' password='pass' host='172.16.0.1' port='5432'"
 
 
-d = r"C:\telematica\oracle"
+d = r"C:\dir\oracle"
 oracledb.init_oracle_client(lib_dir=d)
 
 
